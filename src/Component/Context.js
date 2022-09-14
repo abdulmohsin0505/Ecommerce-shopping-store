866f235772a5fd5c7f1f9122150e3ff1e603e260
abdul mohsin <mohsin050595@gmail.com>
2022-09-14 16:13:47 +0530

    new changes to navbar

--- src/Component/Context.js
+++ src/Component/Context.js
@@ -1,23 +1,35 @@
-import {useContext , useState} from "react"
+import { useContext, useState } from "react"
 import React from "react"
 
 const Context = React.createContext()
 
-  export const ContextProvider = ({children}) =>{
-    const [total , setTotal] = useState(0)
+export const ContextProvider = ({ children }) => {
+    const [total, setTotal] = useState(0)
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const [closBtn, setCloseBtn] = useState(false)
 
     const TotalCartPrice = (carts) => {
         let totalPrice = 0;
-       carts.map(cart => totalPrice  += cart.price * cart.quantity)
-       setTotal(totalPrice.toFixed(2))
+        carts.map(cart => totalPrice += cart.price * cart.quantity)
+        setTotal(totalPrice.toFixed(2))
     }
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+      }
+      const closeSidebar = () => {
+        setIsSidebarOpen(false)
+      }
+    
 
     return (
         <Context.Provider
-        value={{
-            TotalCartPrice,
-            total,
-        }}
+            value={{
+                TotalCartPrice,
+                total,
+                isSidebarOpen,
+                openSidebar,
+                closeSidebar
+            }}
         >
             {children}
         </Context.Provider>
